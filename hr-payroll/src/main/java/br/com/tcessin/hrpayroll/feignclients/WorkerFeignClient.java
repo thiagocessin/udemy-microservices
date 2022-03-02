6f441e3be82d0193cff70da4f7cece7e1bcc0457
thiagocessin <thiagoacessin@gmail.com>
2022-03-02 11:38:45 -0300

    Feign Client

---
+++ hr-payroll/src/main/java/br/com/tcessin/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,19 @@
+package br.com.tcessin.hrpayroll.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.com.tcessin.hrpayroll.entities.Worker;
+
+
+@Component
+@FeignClient(name = "hr-worker", url = "http://localhost:8080", path = "/workers")
+public interface WorkerFeignClient {
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id);
+
+}
