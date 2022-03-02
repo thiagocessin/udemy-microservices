bdb285d39b8fc5c168d56df2783e90fac33049cf
thiagocessin <thiagoacessin@gmail.com>
2022-03-02 10:57:57 -0300

    Criacao do projeto hr-payroll

---
+++ hr-payroll/src/main/java/br/com/tcessin/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,33 @@
+package br.com.tcessin.hrpayroll.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tcessin.hrpayroll.entities.Payment;
+import br.com.tcessin.hrpayroll.services.PaymentService;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentResource {
+
+	
+	@Autowired
+	private PaymentService service;
+	
+	@GetMapping("/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(
+			@PathVariable Long workerId,
+			@PathVariable Integer days) {
+
+		 Payment payment = service.getPayment(workerId, days);
+		 
+		 return ResponseEntity.ok(payment);
+
+	}
+	
+	
+}
---
+++ hr-payroll/src/main/java/br/com/tcessin/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package br.com.tcessin.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import br.com.tcessin.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+	
+}
