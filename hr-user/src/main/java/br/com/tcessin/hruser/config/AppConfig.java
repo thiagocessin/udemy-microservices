bd6cc770fdf5f1de5d745795f476498ad7173a61
thiagocessin <thiagoacessin@gmail.com>
2022-04-01 15:40:48 -0300

    Database insert

---
+++ hr-user/src/main/java/br/com/tcessin/hruser/config/AppConfig.java
@@ -0,0 +1,16 @@
+package br.com.tcessin.hruser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public BCryptPasswordEncoder passworEncoder() {
+		return new BCryptPasswordEncoder();
+
+	}
+
+}
