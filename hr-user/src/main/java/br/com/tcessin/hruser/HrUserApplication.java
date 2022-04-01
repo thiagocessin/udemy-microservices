9353c139a70da2db40da201cb4f9b17032f01658
thiagocessin <thiagoacessin@gmail.com>
2022-04-01 13:03:25 -0300

    User role association

--- hr-user/src/main/java/br/com/tcessin/hruser/HrUserApplication.java
+++ hr-user/src/main/java/br/com/tcessin/hruser/HrUserApplication.java
@@ -1,4 +1,4 @@
-package br.com.tcessin.hrconfigserver;
+package br.com.tcessin.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
